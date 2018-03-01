a4d12ad1e2d0b1d0635cae7ba2d745e335eab1f8
Jeet Gajjar <j211668@gmail.com>
2018-03-01 13:04:26 -0500

    fixing issue

--- js/resume.js
+++ js/resume.js
@@ -18,7 +18,7 @@ function viewModel() {
                       "Aug 2017 - Present"
                   ],
                   bullets: [
-                      "Regularly attend student computer programming competitions as official representative of national organizing body, Major League Hacking (MLH)"
+                      "Regularly attend student computer programming competitions as official representative of national organizing body, Major League Hacking (MLH)",
                           "Act as primary primary point of contact for university, student & faculty volunteers, and event attendees while on-site",
                           "Regular public speaking experience in front of 500+ people.  Presentations involve communicating technical topics to audience of varying skill levels",
                           "each developers how to solve a wide variety of programming challenges and actively help them debug problems across a variety of languages & platforms"
